77efee5be1b344e952d35d74ff967215cd7e9efd
Daniel Berger <76453871+DanielBerg932@users.noreply.github.com>
2022-02-16 14:43:33 +0100

    add  cors config

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
-Access-Control-Allow-Origin: https://priceless-montalcini-251e9b.netlify.app/
-      Access-Control-Allow-Credentials: true
-      Access-Control-Allow-Methods: GET
-      Access-Control-Allow-Headers: Content-Type
 
 app.use(
     cors({
         credentials: true,
-        origin: 'https://brave-boyd-6c3c10.netlify.app'
+        origin: 'https://priceless-montalcini-251e9b.netlify.app'
     })
 );
 app.options('*', cors());
